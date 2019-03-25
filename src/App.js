9eeebd7dcd8838a8e8499fb60b58cb24b5ba5679
NoemiRozpara <nrozpara@gmail.com>
2019-03-25 11:41:10 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Platform, View } from 'react-native';
+import { Provider, Button } from 'react-native-paper';
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const code = `
+  <Provider>
+    <React.Fragment>
+      {Platform.OS === 'web' ? (
+        <style type="text/css">{\`
+        @font-face {
+          font-family: 'MaterialIcons';
+          src: url(${require('react-native-vector-icons/Fonts/MaterialIcons.ttf')}) format('truetype');
+        }
+      \`}</style>
+      ) : null}
+      <View>
+        <Button icon="add-a-photo" onPress={() => console.log('pressed')}>Button</Button>
+      </View>
+    </React.Fragment>
+  </Provider>
+`;
+
+const scope = { Platform, View, Provider, Button };
+
+const App = () => (
+  <LiveProvider code={code} scope={scope}>
+    <LiveEditor />
+    <LiveError />
+    <LivePreview />
+  </LiveProvider>
+);
 
 export default App;
